96850fd79e472ef1fc68de8a35fd2a5228368e21
Soid23 <dorrismutahi@gmail.com>
2024-08-14 12:02:29 +0300

    creating user model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-"""
-user's database
+"""The `user` model's module.
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
